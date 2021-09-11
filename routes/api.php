4d6f5515c70e0033ffb1e277f379e7826ec4ff62
com <cocaomouse@gmail.com>
2021-09-11 14:29:25 +0000

    生成短信验证码

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\VerificationCodesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::prefix('v1')->name('api.v1.')->group(function() {
-
+    // 短信验证码
+    Route::post('verificationCodes',[VerificationCodesController::class,'store'])
+        ->name('verificationCodes.store');
 });
 
